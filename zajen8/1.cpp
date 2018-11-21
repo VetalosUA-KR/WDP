9c11fdfe1dbed2c634ac1246bb87d44fd1b61459
Vitalii Voitenko <vetal925@gmail.com>
2018-11-21 16:37:19 +0100

    8zajen(zad-1 zad-2)

--- zajen8/1.cpp
+++ zajen8/1.cpp
@@ -1,9 +1,44 @@
 #include<iostream>
+#include<cstring>
+#include<string>
 
 using namespace std;
 
 int main ()
 {
+
+    char napis [30];
+    char napis2[30];
+    bool flag = true;
+
+    cin.get(napis,30);
+    int g = 0;
+    for (int i = 0, y = strlen(napis); i < strlen(napis); i++, y--)
+    {
+        if(napis[i]>='a' && napis[i]<='z')
+        {
+            napis2[g] = napis[i];
+            g++;
+        }
+
+    }
+    napis2[g] = '\0';
+
+    for (int i = 0, y = strlen(napis2)-1; i < strlen(napis2)-1; i++, y--)
+    {
+        if(napis2[i] != napis2[y])
+        {
+            flag = false;
+        }
+    }
+    if(flag)
+    {
+        cout << "napis jest polindrom";
+    }
+    else
+    {
+        cout << "napis nie jest polindrom";
+    }
 
     return 0;
 }
---
+++ zajen8/2.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cstring>
+#include<string>
+
+using namespace std;
+
+int main ()
+{
+
+    char napis[30];
+    char napisSZ[0];
+    cin.get(napis,30);
+
+    for (int i = 0; i < strlen(napis); i ++)
+    {
+        /// chek range latter
+        if(int(napis[i] > 96 && int(napis[i]) < 123))
+        {
+            /// if range more than 122
+            if (int(napis[i]+2 > 122))
+            {
+                /// add to start difference
+                int delta = (int(napis[i]+2))%122;
+                napisSZ[i] = char(int(96+delta));
+            }
+            else
+            {
+                napisSZ[i] = char(int(napis[i]+2));
+            }
+        }
+    }
+
+    for (int i = 0; i < strlen(napisSZ); i ++)
+    {
+        cout << napisSZ[i];
+    }
+
+    return 0;
+}
